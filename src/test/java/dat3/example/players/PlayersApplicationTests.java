ba66c46bd36d12468e51e2296af9f20c6feea367
tLangelo <theislangelo@gmail.com>
2022-08-25 15:01:56 +0200

    cmnt

--- src/test/java/dat3/example/players/PlayersApplicationTests.java
+++ src/test/java/dat3/example/players/PlayersApplicationTests.java
@@ -3,10 +3,10 @@ package dat3.example.players;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class PlayersApplicationTests {
 
-    @Test
+    //@Test
     void contextLoads() {
     }
 
